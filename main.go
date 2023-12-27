a0c367ba9e9bd3289577382268fa60080483706b
Paul Walker <paulwalker@paulwalker.dev>
2023-12-27 10:49:24 -0500

    Move file checking into GetLatestRelease()

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,29 +10,28 @@ import (
 
 // GetLatestRelease TODO: Make this function use os_list_v3.json
 func GetLatestRelease() string {
-	resp, err := http.Get("https://downloads.raspberrypi.com/raspios_lite_arm64/release_notes.txt")
-	defer resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := bufio.NewReader(resp.Body)
-	verRaw, err := buf.ReadBytes(':')
-	if err != nil {
-		log.Fatal(err)
-	}
-	return string(verRaw[:len(verRaw)-1])
-}
-
-func main() {
 	releaseRaw, err := os.ReadFile("/srv/release")
 	if err != nil {
 		log.Println("Downloading Raspberry Pi OS release notes")
-		releaseRaw = []byte(GetLatestRelease())
-		err := os.WriteFile("/srv/release", releaseRaw, 0o644)
+		resp, err := http.Get("https://downloads.raspberrypi.com/raspios_lite_arm64/release_notes.txt")
+		defer resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		buf := bufio.NewReader(resp.Body)
+		releaseRaw, err := buf.ReadBytes(':')
+		releaseRaw = releaseRaw[:len(releaseRaw)-1]
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = os.WriteFile("/srv/release", releaseRaw, 0o644)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	release := string(releaseRaw)
-	log.Printf("Base Layer is version: %s\n", release)
+	return string(releaseRaw)
+}
+
+func main() {
+	fmt.Println(GetLatestRelease())
 }
